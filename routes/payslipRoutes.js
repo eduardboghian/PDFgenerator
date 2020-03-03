e55c3bbdb3f2811f216f889d3877c114a2fb5b87
eduardboghian <ediboghian@yahoo.com>
2020-03-03 18:11:03 +0200

    ok

--- routes/payslipRoutes.js
+++ routes/payslipRoutes.js
@@ -6,19 +6,19 @@ router.post('/generate-payslip', async (req, res)=> {
     const data = JSON.parse(req.body)
 
     const responsePDF = await Promise.all(data.map(async data => {
-        const payslips = await Payslip.findOne({ date: data.Date, name: data.Name })
-        if(payslips) {
-        }else {
-            let payslip = new Payslip({
-                date: data.Date,
-                name: data.Name,
-                data: data
-            })
-            payslip = await payslip.save()
-        }
+        // const payslips = await Payslip.findOne({ date: data.Date, name: data.Name })
+        // if(payslips) {
+        // }else {
+        //     let payslip = new Payslip({
+        //         date: data.Date,
+        //         name: data.Name,
+        //         data: data
+        //     })
+        //     payslip = await payslip.save()
+        // }
 
         return await generatePayslipPDF(data)
-      }))
+    }))
       
       res.send(responsePDF)
 })
