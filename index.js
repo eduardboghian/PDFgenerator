2512eb0e205ee9ccde615da8c8d716d22b3cd699
eduardboghian <ediboghian@yahoo.com>
2020-03-17 20:32:36 +0200

    da

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ if (cluster.isMaster) {
     console.log(`Master ${process.pid} is running`);
   
     // Fork workers.
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < numCPUs; i++) {
       cluster.fork();
     }
   
