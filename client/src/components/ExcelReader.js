fd35352a4e7b2b19b86774ff4f03a84e5f9c26bb
eduardboghian <ediboghian@yahoo.com>
2020-03-20 13:33:04 +0200

    sa

--- client/src/components/ExcelReader.js
+++ client/src/components/ExcelReader.js
@@ -114,13 +114,42 @@ class ExcelReader extends Component {
                 link.download = `Payslip-week-ending-${res.data[0].Date}-${res.data[0].Name}.pdf`;
                 link.href = 'data:application/octet-stream;base64,' + b64;
                 document.body.appendChild(link);
-                //link.click()
+                link.click()
                 link.remove()
             })
             .catch(err=> {
-              console.log(data)
+              axios({                 
+                method: 'POST',
+                url: `/api/${this.props.path}`, 
+                data: data,
+                responseType: 'stream'
+              })
+              .then(async res=> {
+                  console.log(res)
+                  function arrayBufferToBase64(buffer) {
+                    let binary = '';
+                    let bytes = new Uint8Array(buffer);
+                    let len = bytes.byteLength;
+                    for (let i = 0; i < len; i++) {
+                        binary += String.fromCharCode(bytes[i]);
+                    }
+                    return window.btoa(binary);
+                  }
+                  let b64 = arrayBufferToBase64( await res.data[1].data)
+  
+                  let link = document.createElement('a');
+                  link.innerHTML = `${res.data[0].Name}`;
+                  link.download = `Payslip-week-ending-${res.data[0].Date}-${res.data[0].Name}.pdf`;
+                  link.href = 'data:application/octet-stream;base64,' + b64;
+                  document.body.appendChild(link);
+                  link.click()
+                  link.remove()
+              })
+              .catch(err=> {
+                
+                console.log(err)
+              })
               console.log(err)
-              
             })
           })
 
